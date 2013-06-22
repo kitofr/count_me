eb53905cdb10986af560c4e45d6c63c7cb3e3985
Kristoffer Roupe <kristoffer.roupe@cint.com>
2013-06-22 23:03:04 +0200

    Removed question from model

--- src/count.js
+++ src/count.js
@@ -2,12 +2,10 @@ angular.module('countme', ['ui.bootstrap']);
 
 function QuestionCtrl($scope){
   var numbers = {
-    question: 1,
     alternatives: [1,2,3,4,5]
   };
 
   var roman = {
-    question: "I",
     alternatives: ["I", "II", "III", "IV", "V"]
   };
 
@@ -15,17 +13,17 @@ function QuestionCtrl($scope){
 
   $scope.clicked = function(alternative){
     $scope.lastAnswer = alternative;
-    $scope.message = $scope.isCorrect() ? "You guessed correct!" : alternative + " is not " + $scope.model.question;
+    $scope.message = $scope.isCorrect() ? "You guessed correct!" : alternative + " is not the same as " + $scope.question;
   };
 
   $scope.isCorrect = function(){
-    return $scope.model.question === $scope.lastAnswer;
+    return $scope.question === $scope.lastAnswer;
   };
 
   $scope.newQuestion = function(){
     $scope.message = "";
     $scope.lastAnswer = undefined;
     var index = Math.floor(Math.random() * 11) % $scope.model.alternatives.length;
-    $scope.model.question = $scope.model.alternatives[index];
+    $scope.question = $scope.model.alternatives[index];
   };
 }
